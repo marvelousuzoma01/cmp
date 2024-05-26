a71a88accfcd482e74b44051223d7a6fc099372d
marvelousuzoma01 <130617409+marvelousuzoma01@users.noreply.github.com>
2024-05-26 20:38:17 +0100

    Update Main.java

--- java_class_barka/Main.java
+++ java_class_barka/Main.java
@@ -1,17 +1,17 @@
 package java_class_barka;
 
 public class Main {
-    String name = "john doe";
+    String name ="bola";
     String password;
     String schoolName;
     String matricNo;
     int staffNo;
     public static void main(String[] args) {
-        Person abigail = new Person("binghamUni", "ajiri", "xyz1234", "098");
+        Person abigail = new Person("binghamUni", "uzoma", "xyz1234", "098");
         Person lecturer = new Person("uniBingham", "barka", "xyz1234", 0064);
 
-        abigail.setName("rokari");
-        abigail.setPass("xyzzabc");
+        abigail.setName("marvelous");
+        abigail.setPass("1234abc");
         System.out.println(abigail.getName());
         System.out.println(lecturer.getName());
         System.out.println(abigail.getPass());
